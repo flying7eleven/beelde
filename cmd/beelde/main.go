2ec62e20c83e152e13510e24273525f7258d6c9a
Tim Hütz <tim@huetz.biz>
2019-06-24 11:05:33 +0200

    chore: add a middle ware for gin-gonic for using logrus

--- cmd/beelde/main.go
+++ cmd/beelde/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"beelde/internal/pkg/utils"
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -10,8 +12,9 @@ func HealthStatus(context *gin.Context) {
 	context.Status(http.StatusNoContent)
 }
 
-func createRouter() *gin.Engine {
-	router := gin.Default()
+func createRouter(log *logrus.Logger) *gin.Engine {
+	router := gin.New()
+	router.Use(utils.GinLogger(log), gin.Recovery())
 	router.Use(cors.AllowAll())
 
 	v1 := router.Group("/v1")
@@ -23,8 +26,11 @@ func createRouter() *gin.Engine {
 }
 
 func main() {
+	// initialize our logging instance
+	logger := logrus.New()
+
 	// get the actual router for the whole API
-	router := createRouter()
+	router := createRouter(logger)
 
 	// start serving the API
 	_ = router.Run(":8080")
