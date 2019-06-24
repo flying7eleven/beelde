6d78f4edfaf1050461af39605efd839b2d001db2
Tim Hütz <tim@huetz.biz>
2019-06-24 09:06:19 +0200

    chore: add a very simple REST example

--- cmd/beelde/main.go
+++ cmd/beelde/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	orm "github.com/jinzhu/gorm"
+	cors "github.com/rs/cors/wrapper/gin"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func main() {
-	log.Info("Hello, World!")
+	router := gin.Default()
+	router.Use(cors.AllowAll())
+
+	v1 := router.Group("/v1")
+
+	{
+		v1.GET("/user/:name", func(c *gin.Context) {
+			name := c.Param("name")
+			c.String(http.StatusOK, "Hello %s", name)
+		})
+
+		v1.GET("/db", func(c *gin.Context) {
+			log.Info("Hello, World!")
+			db, err := orm.Open("sqlite3", "test.db")
+			if err != nil {
+				panic("failed to connect database")
+			}
+			defer db.Close()
+			c.Status(http.StatusNoContent)
+		})
+	}
+
+	router.Run(":8080")
+
 }
