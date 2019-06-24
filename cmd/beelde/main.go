8137263e713d594f9620ff08c401b0bed7e5eb2f
Tim Hütz <tim@huetz.biz>
2019-06-24 10:47:55 +0200

    chore: remove some test code and start to clean up the main

--- cmd/beelde/main.go
+++ cmd/beelde/main.go
@@ -2,35 +2,31 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	orm "github.com/jinzhu/gorm"
 	cors "github.com/rs/cors/wrapper/gin"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
-func main() {
+func HealthStatus(context *gin.Context) {
+	context.Status(http.StatusNoContent)
+}
+
+func createRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.AllowAll())
 
 	v1 := router.Group("/v1")
-
 	{
-		v1.GET("/user/:name", func(c *gin.Context) {
-			name := c.Param("name")
-			c.String(http.StatusOK, "Hello %s", name)
-		})
-
-		v1.GET("/db", func(c *gin.Context) {
-			log.Info("Hello, World!")
-			db, err := orm.Open("sqlite3", "test.db")
-			if err != nil {
-				panic("failed to connect database")
-			}
-			defer db.Close()
-			c.Status(http.StatusNoContent)
-		})
+		v1.GET("/health", HealthStatus)
 	}
 
-	router.Run(":8080")
+	return router
+}
+
+func main() {
+	// get the actual router for the whole API
+	router := createRouter()
+
+	// start serving the API
+	_ = router.Run(":8080")
 
 }
